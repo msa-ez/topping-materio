43cdb46f345e31e88f62a19933005c618aedad62
kyusoo <rbtn110@uengine.org>
2023-11-30 15:23:56 +0900

    handlebars 수정

--- for-model/frontend-materio/javascript-version/src/views/components/ui/ValueObjectDetailGrid.vue
+++ for-model/frontend-materio/javascript-version/src/views/components/ui/ValueObjectDetailGrid.vue
@@ -118,7 +118,6 @@ export default {
         {{getPrimitiveType className}},
         {{/if}}
         {{else}}
-        {{namePascalCase}},
         {{/if}}
         {{/if}}
         {{/fieldDescriptors}}
